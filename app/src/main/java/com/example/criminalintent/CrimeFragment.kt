7338906f25aa274bc2f584c35e21cada9317e610
Liinadec <Liinadec@gmail.com>
2021-05-06 16:12:42 +0300

    work commit 06.05.21

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -6,6 +6,8 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
@@ -13,6 +15,8 @@ class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
     private lateinit var crimeEt:EditText
+    private lateinit var dateBtn:Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,13 @@ class CrimeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
 
         crimeEt = view.findViewById(R.id.crime_title_Et) as EditText
+        dateBtn = view.findViewById(R.id.crime_date_Btn) as Button
+
+        dateBtn.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+        solvedCheckBox = view.findViewById(R.id.crime_solved)
 
         return view
     }
@@ -38,6 +49,7 @@ class CrimeFragment : Fragment() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 crime.title = s.toString()
+
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -46,6 +58,9 @@ class CrimeFragment : Fragment() {
 
         }
 
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { buttonView, isChecked -> crime.isSolved = isChecked  }
+        }
     }
 
 }
