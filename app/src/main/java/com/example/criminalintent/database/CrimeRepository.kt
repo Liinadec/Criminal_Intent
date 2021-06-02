b2069011a2228989b48985f7c83ec99c20571cab
Liinadec <Liinadec@gmail.com>
2021-06-02 17:23:27 +0300

    10 Database ver 3

--- app/src/main/java/com/example/criminalintent/database/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/database/CrimeRepository.kt
@@ -1,4 +1,31 @@
 package com.example.criminalintent.database
 
-class CrimeRepository {
+import android.content.Context
+import androidx.room.Room
+
+private const val DATABASE_NAME = "crime database"
+
+class CrimeRepository private constructor(context: Context){
+
+    private val database:CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
+
+    companion object{
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context){
+            if(INSTANCE == null){
+                INSTANCE == CrimeRepository(context)
+            }
+        }
+
+        fun get():CrimeRepository{
+            return INSTANCE ?: throw IllegalStateException("Crime repository must be initialized")
+        }
+    }
 }
